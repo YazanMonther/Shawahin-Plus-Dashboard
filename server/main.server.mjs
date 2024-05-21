0ffa5262b69b92b7b02942dfcda31a9e7a6e7e44
Yazan Alhelo <116039946+YazanMonther@users.noreply.github.com>
2024-05-22 02:57:07 +0400

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2YBIXKUB.mjs";import"./chunk-CHOL4B2P.mjs";import"./chunk-7I7L2KFM.mjs";import"./chunk-65BLW562.mjs";import"./chunk-2BUOXUC6.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
